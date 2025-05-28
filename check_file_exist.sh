1c5f68a8f51dc6c7475f35b266edb87691a236f0
a-karim2004 <karimdansalao@gmail.com>
2025-05-28 10:04:58 +0100

    Delete check_file_exist.sh

--- check_file_exist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Ce script vérifie si un fichier donné existe
-read -p "Entrez le nom du fichier : " filename
-if [ -f "$filename"  ]; then
-echo "Le fichier '$filename' existe"
-else
-echo "Le fichier '$filename' n'existe pas"
-fi
